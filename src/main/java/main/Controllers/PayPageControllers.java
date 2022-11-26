3bae5b7b75a388c9a81d3365de2e619d3585270c
EngEng <england.thanarat@gmail.com>
2022-11-26 22:34:25 +0700

    Close Receipt

--- src/main/java/main/Controllers/PayPageControllers.java
+++ src/main/java/main/Controllers/PayPageControllers.java
@@ -1,8 +1,15 @@
 package main.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class PayPageControllers {
 
@@ -10,7 +17,15 @@ public class PayPageControllers {
         private Button Pay;
 
         @FXML
-        void PayClick(MouseEvent event) {
+        void PayClick(MouseEvent event) throws IOException {
+                Parent root = null;
 
+                root = FXMLLoader.load(getClass().getResource("Receipt.fxml"));
+                Scene scene = new Scene(root);
+
+                Stage stage = new Stage();
+                stage.initStyle(StageStyle.TRANSPARENT);
+                stage.setScene(scene);
+                stage.show();
         }
 }
--- src/main/java/main/Controllers/ReceiptControllers.java
+++ src/main/java/main/Controllers/ReceiptControllers.java
@@ -2,26 +2,10 @@ package main.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-public class BookingReceipt {
-
-    @FXML
-    private Label BillNumber;
-
-    @FXML
-    private Label CustomerName;
-
-    @FXML
-    private Label DateNumber;
-
-    @FXML
-    private Label TableNumber;
-
-    @FXML
-    private Label TelephoneNumber;
+public class ReceiptControllers {
 
     @FXML
     private Button close;
